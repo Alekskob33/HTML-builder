7d8c290755af9d43e2e4d93852c091efae315cfe
alekskob33 <alekskob33@yandex.ru>
2022-05-23 21:25:17 +0300

    fix: error remove folder when live-server 'on'

--- 06-build-page/copyDir.js
+++ 06-build-page/copyDir.js
@@ -7,10 +7,21 @@ async function copyDir(srcPath, distPath) {
   srcPath = path.resolve(__dirname, srcPath); // 'files/'
   distPath = path.resolve(__dirname, distPath); // 'project-dist/assets/'
 
+  // init dist
   try {
-    // init dist
-    await rm(distPath, { recursive: true, force: true });
-    await mkdir(distPath, { recursive: true });
+    await rm(distPath, { recursive: true, force: true, maxRetries: 20 });
+  } catch (err) {
+    console.log(`
+    * * * 
+    Остановите live-server. После чего запустите команду сборки проекта.
+    Ссылка в закрепленное сообщение в discord-канале: 
+    https://discord.com/channels/516715744646660106/974921759969124362/978235566137172009
+    `);
+    process.exit();
+  }
+  await mkdir(distPath, { recursive: true });
+
+  try {
 
     const files = await readdir(srcPath, { withFileTypes: true });
     for (let file of files) {
