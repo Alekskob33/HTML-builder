5fac5279cd14c901e7bdcdbff9a51211dc774c83
alekskob33 <alekskob33@yandex.ru>
2022-05-23 16:56:41 +0300

    feat: solve '01 read file'

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const filePath = path.resolve(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, 'utf-8');
+let text = '';
+
+readStream.on('data', chunk => text += chunk);
+readStream.on('end', () => stdout.write(text));
